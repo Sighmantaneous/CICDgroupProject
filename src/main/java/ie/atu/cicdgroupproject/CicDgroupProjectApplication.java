84c4cb8608a764fec6d68ef52a941d1b284ba16e
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-10-08 14:23:37 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/cicdgroupproject/CicDgroupProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDgroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicDgroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicdgroupproject/CicDgroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDgroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
